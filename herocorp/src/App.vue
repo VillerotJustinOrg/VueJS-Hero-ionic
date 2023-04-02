bc3c35f879de770b89fbbaf49fec3de5fcef8e98
Villerot Justin <justinvillerot@orange.fr>
2023-04-02 17:52:12 +0200

    Reset 999

--- herocorp/src/App.vue
+++ herocorp/src/App.vue
@@ -1,27 +1,9 @@
 <template>
   <ion-app>
-    <NavBar />
-    <ErrorDialog></ErrorDialog>
     <ion-router-outlet />
   </ion-app>
 </template>
 
-<script setup>
-import { IonApp, IonRouterOutlet} from "@ionic/vue";
-import NavBar from "@/components/Navbar.vue";
-import { mapActions } from "vuex";
-import ErrorDialog from "@/components/ErrorDialog.vue";
-export default {
-  name: "App",
-  components: { ErrorDialog, NavBar },
-  data: () => ({
-    //
-  }),
-  methods: {
-    ...mapActions("data", ["loading"]),
-  },
-  created() {
-    this.loading();
-  },
-};
+<script setup lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
